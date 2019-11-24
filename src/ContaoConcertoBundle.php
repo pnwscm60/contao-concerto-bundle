d2aff97f1705acbc32f55eeb25b60732fcd43bbd
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-24 10:30:04 +0100

    Create ContaoConcertoBundle.php

---
+++ src/ContaoConcertoBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Pnwscm60\ContaoConcertoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoConcertoBundle extends Bundle
+{
+}
