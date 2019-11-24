577e01de62bfd4eba57ac17611c3e09851b229fb
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-24 11:36:07 +0100

    Update ContaoConcertoExtension.php

--- src/DependencyInjection/ContaoConcertoExtension.php
+++ src/DependencyInjection/ContaoConcertoExtension.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
  
- namespace Pnwscm60\ContaoConcertoBundle\DependencyInjection;
+namespace Pnwscm60\ContaoConcertoBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
