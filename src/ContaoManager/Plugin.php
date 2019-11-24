2d393b3e055a4ff0ff694faabed5a5855e163fb8
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-24 10:32:26 +0100

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Concerto.
+ *
+ * (c) Markus Schenker
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Pnwscm60\ContaoConcertoBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Pnwscm60\ContaoConcertoBundle\ContaoConcertoBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoConcertoBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
