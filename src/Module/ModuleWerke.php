bf444349e4a042b0111d430b1080b653dfddd384
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-25 14:59:56 +0100

    Update ModuleWerke.php

--- src/Module/ModuleWerke.php
+++ src/Module/ModuleWerke.php
@@ -30,46 +30,12 @@ class ModuleWerke extends \Contao\Module
         //$this->import('FrontendUser', 'User');
 	//	$userid = $this->User->id;
         
-        /*** EDITIERTE DATEN SPEICHERN ***/
-        if($_REQUEST['trg']=='swerked'){
-            $cid = $_REQUEST['cid'];
-            $title = $_REQUEST['title'];
-            $komponist = $_REQUEST['komponist'];
-            $besetzung = $_REQUEST['besetzung'];
-            $db->update('catalog', array('title' => $title, 'komponist' => $komponist, 'besetzung' => $besetzung), array('id' => $cid));
-            $sedit = '1';
-        }
-        /*** ZU EDITIERENDE DATEN ABRUFEN ***/
-        if($_REQUEST['trg']=='werked'){
-            $catid = $_REQUEST['catid']; // key
-            $sql="SELECT tl_catalog.id as cid, komponist, title, besetzung FROM tl_catalog WHERE tl_catalog.id = ?";
-		    $stat = $db->prepare($sql);
-            $stat->bindValue(1, $catid);
-            $objCat = $stat->fetch();
-            $this->Template->cid = $objCat->cid;
-            $this->Template->title = $objCat->title;
-            $this->Template->komponist = $objCat->komponist;
-            $this->Template->besetzung = $objCat->besetzung;
-            $this->Template->todo = 'edcat';
-        }
+       
         
         /*** WERKLISTE ***/
         echo "Werkliste";
            
-        /*** WERK LÖSCHEN ***/
-            if($_REQUEST['trg']=='delcat'){
-            /*    
-            $cid = $_REQUEST['cid'];
-            $sql='DELETE from tl_category WHERE id='.$cid.';'; 
-		  $objResult = \Database::getInstance()->execute($sql);
-            $this->Template->mess = "Kategorie wurde erfolgreich gelöscht";
-            */
-            echo '<script type="text/javascript"> 
-            alert ( "Löschen des Werks führt zu Dateninkonsistenz. Bitte um Löschung an Admin melden." );
-            location.href = "admin.html?trg=admin&todo=clist";
-            </script>';
-            exit;
-        }
+       
     }
 }
 ?>
