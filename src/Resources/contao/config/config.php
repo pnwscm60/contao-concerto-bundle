481d7a62904c1341c698cec4045431c7e9e69785
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-28 00:04:52 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,18 +4,12 @@
 
  * Front end modules
  */
-/*
-array_insert($GLOBALS['FE_MOD'], 4, array
-(
-	'concerto' => array
-	(
-		'werke'   => 'Pnwscm60\ConcertoBundle\ModuleWerke'
-	)
-));
-*/
-use Pnwscm60\ConcertoBundle\Module\ModuleWerke; 
+
+use Pnwscm60\ConcertoBundle\Module\ModuleWerke;
+use Pnwscm60\ConcertoBundle\Module\ModuleEnsemble;
 
 $GLOBALS['FE_MOD']['concerto'] = [ 
-    'werke' => ModuleWerke::class, 
+    'werke' => ModuleWerke::class,
+	'ensemble' => ModuleWerke::class,
 ];  
 
