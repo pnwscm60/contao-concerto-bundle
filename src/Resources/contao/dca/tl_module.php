2ec4e1a72763eeb370e69963966681a4d04643e9
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-28 00:24:54 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,4 +3,4 @@
  * Add a palette to tl_module
  */
 $GLOBALS['TL_DCA']['tl_module']['palettes']['werke'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-?>
+$GLOBALS['TL_DCA']['tl_module']['palettes']['ensemble'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
