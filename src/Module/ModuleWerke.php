b0d23cf67e5ad1e42ce859e55e1f2b6437b6a949
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-11-27 02:02:08 +0100

    Update ModuleWerke.php

--- src/Module/ModuleWerke.php
+++ src/Module/ModuleWerke.php
@@ -25,15 +25,26 @@ class ModuleWerke extends \Contao\Module
 	{
 	/* Datenbank abrufen*/
 	$this->import('Database');
-		
+	
+	/* EDITIERTES WERK SPEICHERN*/
+	if($_REQUEST['trg']=='swerked'){
+            $cid = $_REQUEST['cid'];
+            $title = $_REQUEST['title'];
+            $komponist = $_REQUEST['komponist'];
+			$komponistvn = $_REQUEST['komponistvn'];
+            $besetzung = $_REQUEST['besetzung'];
+            $set = "title = '$title', komponist = '$komponist', komponistvn = '$komponistvn', besetzung = '$besetzung'";
+            $this->Database->prepare("UPDATE tl_catalog $set WHERE id=$cid;)->execute();
+			$sedit = '1';
+        }
 	/*** ZU EDITIERENDE DATEN ABRUFEN ***/
         if($_REQUEST['trg']=='werked'){
             $catid = $_REQUEST['cid']; // key
             $sql="SELECT tl_catalog.id as cid, komponist, komponistvn, title, besetzung FROM tl_catalog WHERE tl_catalog.id = $catid;";
-		$result = $this->Database->prepare($sql)->execute();
+			$result = $this->Database->prepare($sql)->execute();
             	$this->Template->cid = $result->cid;
             	$this->Template->title = $result->title;
-		$this->Template->komponistvn = $result->komponistvn;
+				$this->Template->komponistvn = $result->komponistvn;
             	$this->Template->komponist = $result->komponist;
             	$this->Template->besetzung = $result->besetzung;
             	$this->Template->todo = 'edcat';
