8b29588f8f76c6f3ccaa6f9407cc3c765db86bc0
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-12-02 16:33:16 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -4,3 +4,4 @@
  */
 $GLOBALS['TL_DCA']['tl_module']['palettes']['werke'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['ensemble'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['concert'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
