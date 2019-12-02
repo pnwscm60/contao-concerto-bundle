a77380c745fff1141ad64d9c7785611a3c9d1add
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-12-02 16:32:26 +0100

    Create tl_concert.php

---
+++ src/Resources/contao/dca/tl_concert.php
@@ -0,0 +1,134 @@
+<?php
+/*
+** DCA für concerto: tl_concert
+** © 2019 Markus Schenker, Phi Network
+*/
+/*
+ * Table tl_concert
+ */
+$GLOBALS['TL_DCA']['tl_concert'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary'
+			)
+		),
+	),
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 1,
+			'fields'                  => array('title'),
+			'flag'                    => 8,
+			'panelLayout'             => 'filter;sort,search,limit'
+		),
+		'label' => array
+		(
+			'fields'                  => array('title'),
+			'format'                  => '%s',
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_concert']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_concert']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_concert']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif',
+				'attributes'          => 'style="margin-right:3px"'
+			),
+		)
+	),
+	// Palettes
+	'palettes' => array
+	(
+		'default'	=> 'title;director;solisti;werkids;ensembleid;'
+	),
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'tstamp' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'title' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_concert']['title'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'director' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_concert']['director'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'solisti' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_concert']['solisti'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'werkids' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_concert']['werkids'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+	)
+);
