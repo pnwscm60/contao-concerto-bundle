026ca298320c670eecb9a7d3c4ffd321986bfd87
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-12-06 00:18:57 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,10 +8,12 @@
 use Pnwscm60\ConcertoBundle\Module\ModuleWerke;
 use Pnwscm60\ConcertoBundle\Module\ModuleEnsemble;
 use Pnwscm60\ConcertoBundle\Module\ModuleConcert;
+use Pnwscm60\ConcertoBundle\Module\ModuleConcertlist;
 
 $GLOBALS['FE_MOD']['concerto'] = [ 
     'werke' => ModuleWerke::class,
 	'ensemble' => ModuleEnsemble::class,
-	'concert' => ModuleConcert::class
+	'concert' => ModuleConcert::class,
+	'concertlist' => ModuleConcertlist::class
 ];  
 
